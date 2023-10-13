281545b728f2c42734199eb74c40415ddf0579e2
humingjie8 <105139344+humingjie8@users.noreply.github.com>
2023-10-13 21:16:38 +0800

    Add files via upload

---
+++ stand_v1.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import seaborn as sns
+import openpyxl
+from tqdm import tqdm
+
+# 使用切片来获取前N项并计算它们的和
+def sum_of_first_n_elements(arr, n):
+    return sum(arr[:n])
+
+
+#设置总样本数据量
+number_data_z = 0
+number_data_t = 1000
+#set for data cycle number
+cycle = 100
+
+# 创建一个新的Excel工作簿
+workbook = openpyxl.Workbook()
+
+# 创建一个工作表（默认名称是Sheet）
+sheet = workbook.active
+sheet.cell(row=1, column=1, value='x')
+sheet.cell(row=1, column=2, value=0.01)
+sheet.cell(row=1, column=3, value=0.05)
+sheet.cell(row=1, column=4, value=0.1)
+
+t = [0] * number_data_t
+for i in tqdm(range(cycle), desc="Processing"):
+    number_data_z += 100
+    for x in range(number_data_t):
+        seed = x
+        # 设置随机数种子以确保可重复性
+        np.random.seed(seed)
+
+        standard_normal_samples = np.random.standard_normal(size=number_data_z)  # 生成10000个样本
+        mean_value = np.mean(standard_normal_samples)
+        min_value = np.min(standard_normal_samples)
+
+        # for i in range(number_data):
+        mid_1 = sum_of_first_n_elements((standard_normal_samples-mean_value)**2, number_data_z)
+        t[x] = (math.sqrt(mid_1/(number_data_z)))/(mean_value-min_value)
+        # print(np.min(t))
+
+    # 计算分位数
+    N1 = np.percentile(t, 0.01)
+    N2 = np.percentile(t, 0.05)
+    N3 = np.percentile(t, 0.1)
+    # print(N1, N2, N3)
+
+    # 在EXCEL中写入数据
+    sheet.cell(row=i+2, column=1, value=str(number_data_z))  # 在第一列写入数据
+    sheet.cell(row=i+2, column=2, value=str(N1))  # 在第2列写入数据    
+    sheet.cell(row=i+2, column=3, value=str(N2))  # 在第3列写入数据
+    sheet.cell(row=i+2, column=4, value=str(N3))  # 在第4列写入数据
+
+    # 绘制KDE图
+    label = 'number_z='+str(number_data_z)
+    sns.kdeplot(t, fill=False, label=label, color='r')
+    plt.title('Kernel Density Estimation (KDE) of Data')
+
+    # 设置横坐标和纵坐标的范围
+    x_max = 1
+    # if number_data > 100 :
+    #     x_max = 0.5
+    plt.xlim(0, x_max)  # 设置横坐标的范围为1到5
+    # plt.ylim(0, 12)  # 设置纵坐标的范围为0到12
+
+    plt.xlabel('Value')
+    plt.ylabel('Density')
+    plt.legend()
+
+    # #SAVE PIC
+    # name_pic = 'stand_pic/'+'stand'+str(number_data_z)+'.png'
+    # plt.savefig(name_pic)
+
+    # plt.show()
+    # plt.clf()
+
+# 保存Excel文件
+name_excel = 'stand'+'.xlsx'
+workbook.save(name_excel)
+
+#SAVE PIC
+# name_pic = 'stand_pic/'+'stand'+'.png'
+# plt.savefig(name_pic)
+
+# 关闭工作簿
+workbook.close()
